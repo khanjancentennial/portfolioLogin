d5328c1b53a4a68539f8bca86b6364adb0098ecd
khanjancentennial <khanjancentennial@gmail.com>
2023-02-26 20:06:50 -0500

    change mongodb connection uri from local to cloud

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI":"mongodb://127.0.0.1:27017/products"
+    // "URI":"mongodb://127.0.0.1:27017/products"
+    "URI":"mongodb+srv://programing:Programing@cluster0.na217oh.mongodb.net/?retryWrites=true&w=majority"
 }
