2c376fd4d51993f5301bbb9f2b0ea46aad220af3
khanjancentennial <khanjancentennial@gmail.com>
2023-02-27 18:44:55 -0500

    uri changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports =
 {
     // "URI":"mongodb://127.0.0.1:27017/products"
-    "URI":"mongodb+srv://programing:Programing@cluster0.na217oh.mongodb.net/?retryWrites=true&w=majority"
+    "URI":"mongodb+srv://programing:Programing@cluster0.na217oh.mongodb.net/test?retryWrites=true&w=majority"
 }
